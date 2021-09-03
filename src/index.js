1d7ffe5bea18a7e401d8ad70249d48416cf04796
Yoursrj <riturajjhacr7@gmail.com>
2021-09-03 21:59:14 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,82 @@
+import React , { Component } from "react";
+import ReactDOM , { render } from "react-dom";
+import Hello from './Hello';
+import './style.css';
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div className="rectangle" >
+
+       <div className="main">
+       <h1>
+          Add Education
+        </h1>
+        <hr/>
+        <form id='form'>
+          <h2>
+            School <br/>
+            <input placeholder = "eg. Oxford" size="75" required />
+          </h2>
+          <h2>
+            Degree <br/>
+            <input placeholder = "eg. Btech" size="75" required />
+          </h2>
+          <h2>
+            Field Of Study <br/>
+            <input placeholder = "eg. Computer Science" size="75" required />
+          </h2>
+          <h2>
+            Grade <br/>
+            <input placeholder = "eg. Percentage" size="75" required />
+          </h2>
+          <h2>
+            Activities and Socities <br/>
+            <input placeholder = "eg. Volleyball,competitions etc" size="75" required />
+          </h2>
+          <h2>
+          Location<br/>
+            <input placeholder = "eg. Tamil Nadu" size="75" required />
+          </h2>
+
+         
+         <h2  className="Start">
+          Start Date <br/>
+            <input placeholder = "Month" type="Date" id = "startdate"  required />
+            
+          </h2>
+
+          <h2  className="end" >
+          End Date <br/>
+            <input placeholder = "Month" type="Date" id = "enddate"  required />
+
+          </h2>
+        
+        
+          
+          <h2>
+            Description <br/>
+            <textarea id="w3review" name="w3review" placeholder = "eg. @Oxford"  rows="6" cols="80" required/>    
+          </h2> 
+        </form>
+        <button form='form' className="Submit">Cancel</button>
+        <button form='form' className="Submit1">Save</button>
+       </div>
+        
+      </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
